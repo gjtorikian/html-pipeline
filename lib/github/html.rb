60fcdbcb01dbac8ccaa6f6f97699b4c9f1d44e55
Ryan Tomayko <rtomayko@gmail.com>
2011-03-19 02:19:08 -0700

    DOM based @mentions filter

--- lib/github/html.rb
+++ lib/github/html.rb
@@ -6,5 +6,27 @@ module GitHub
   module HTML
     require 'github/html/camouflage'
     require 'github/html/sanitization'
+    require 'github/html/@mentions'
+  end
+end
+
+# Work around an issue with Nokogiri::XML::Node#swap and #replace not
+# working on text nodes when the replacement is a document fragment. See
+# #407 in the Nokogiri issue tracker for more info:
+#
+# https://github.com/tenderlove/nokogiri/issues/407
+#
+# This monkey patch should be removed when a new version of Nokogiri
+# is available.
+class Nokogiri::XML::Text < Nokogiri::XML::CharacterData
+  def replace(replacement)
+    temp = add_next_sibling("<span></span>")
+    remove
+    temp.first.replace(replacement)
+  end
+
+  def swap(replacement)
+    replace(replacement)
+    self
   end
 end
