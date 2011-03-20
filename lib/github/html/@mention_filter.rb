72383717c02b57f212f2e854fb24f47c73eb910f
Ryan Tomayko <rtomayko@gmail.com>
2011-03-20 08:56:57 -0700

    move @mention parsing to MentionFilter
    We lose the nice super reusable Mentionable mixin but I don't think
    we'd actually be using it anywhere once we move everything to filter
    based extraction. If we find we need generic parsing we can pull it
    back out.

--- lib/github/html/@mention_filter.rb
+++ lib/github/html/@mention_filter.rb
@@ -1,5 +1,4 @@
 module GitHub::HTML
-
   # HTML filter that replaces @user mentions with links. Mentions within <pre>,
   # <code>, and <a> elements are ignored. Mentions that reference users that do
   # not exist are ignored.
@@ -8,11 +7,14 @@ module GitHub::HTML
   #   :base_url - Used to construct links to user profile pages for each
   #               mention.
   #
-  # This filter does not write additional information to the context (yet).
+  # The following keys are written to the context hash:
+  #   :mentioned_users - An array of User objects that were mentioned.
+  #
   class MentionFilter < Filter
-    include GitHub::Mentionable
+    MentionPattern = /(?:^|\W)@([a-z0-9][a-z0-9-]*)(?=\W|$)/i
 
     def call
+      mentioned_users.clear
       doc.search('text()').each do |node|
         content = node.to_html
         next if !content.include?('@')
@@ -20,6 +22,52 @@ module GitHub::HTML
         html = mention_link_filter(content, base_url)
         next if html == content
         node.replace(html)
+      end
+      mentioned_users.uniq!
+      doc
+    end
+
+    # List of User objects that were mentioned in the document. This is
+    # available in the context hash as :mentioned_users.
+    def mentioned_users
+      context[:mentioned_users] ||= []
+    end
+
+    # Replace user @mentions in text with links to the mentioned user's
+    # profile page.
+    #
+    # text      - String text to replace @mention usernames in.
+    # base_url  - The base URL used to construct user profile URLs.
+    #
+    # Returns a string with @mentions replaced with links. All links have a
+    # 'user-mention' class name attached for styling.
+    def mention_link_filter(text, base_url='/')
+      text.gsub MentionPattern do |match|
+        login = $1
+        user = User.cached_by_login($1)
+        next match if user.nil?
+        next match if !mentionable?(user)
+
+        mentioned_users << user
+        url = File.join(base_url, user.login)
+        link =
+          "<a href='#{url}' class='user-mention'>" +
+          "@#{user.login}" +
+          "</a>"
+        match.sub("@#{login}", link)
+      end
+    end
+
+    # Determine if a user is mentionable in the current context.
+    #
+    #   user - The User object to check
+    #
+    # Returns true when the user may be mentioned
+    def mentionable?(user)
+      if repository
+        repository.pullable_by?(user)
+      else
+        true
       end
     end
   end
