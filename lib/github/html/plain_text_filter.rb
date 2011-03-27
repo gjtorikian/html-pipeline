dd5ad61d6359d557dd23d1e2dd7dee821ea56136
Simon Rozet <simon@rozet.name>
2011-03-27 20:27:37 +0200

    move TextFilter under GitHub::HTML

---
+++ lib/github/html/plain_text_filter.rb
@@ -0,0 +1,37 @@
+module GitHub::HTML
+  module PlainTextFilter
+    def self.call(body)
+      Pipeline.call(body)
+    end
+
+    class Filter < ::GitHub::HTML::Filter
+      def call
+        links = []
+
+        doc.search("a").each do |node|
+          links << node["href"]
+
+          index = links.size
+          label = "#{node.text} [#{index}]"
+
+          node.replace(Nokogiri::XML::Text.new(label, doc))
+        end
+
+        doc.add_child(Nokogiri::XML::Text.new("\n\n", doc))
+
+        links.each_with_index do |link, i|
+          text = "[#{i + 1}]: #{link}\n"
+          doc.add_child(Nokogiri::XML::Text.new(text, doc))
+        end
+      end
+    end
+
+    Pipeline = ::GitHub::HTML::Pipeline.new [
+      lambda { |doc, _| "<p>#{doc}</p>" },
+      ::GitHub::HTML::IssueMentionFilter,
+      ::GitHub::HTML::CommitMentionFilter,
+      Filter,
+      lambda { |doc, _| doc.text }
+    ]
+  end
+end
