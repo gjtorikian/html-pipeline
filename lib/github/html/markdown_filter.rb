c3a557dac0b8f5a704018738b5de40a3217bdcac
Ryan Tomayko <rtomayko@gmail.com>
2011-03-29 11:07:25 -0500

    MarkdownFilter uses redcarpet + axe rdiscount specific logic

--- lib/github/html/markdown_filter.rb
+++ lib/github/html/markdown_filter.rb
@@ -1,3 +1,5 @@
+require 'github/markdown'
+
 module GitHub::HTML
   # HTML Filter that converts Markdown text into HTML and converts into a
   # DocumentFragment. This is different from most filters in that it can take a
@@ -19,13 +21,8 @@ module GitHub::HTML
     # Convert Markdown to HTML using the best available implementation
     # and convert into a DocumentFragment.
     def call
-      html =
-        if rdiscount?
-          flags = (context[:autolink] == false) ? [] : [:autolink]
-          Markdown.new(markdown_text, *flags).to_html
-        else
-          Markdown.new(markdown_text).to_html
-        end
+      flags = (context[:autolink] == false) ? [] : [:autolink]
+      html = GitHub::Markdown.new(markdown_text, *flags).to_html
       @doc = parse_html(html)
     end
 
@@ -40,11 +37,6 @@ module GitHub::HTML
       end
     end
 
-    # Is RDiscount available and set as the default markdown processor?
-    def rdiscount?
-      defined?(::RDiscount) && Markdown == RDiscount
-    end
-
     # TODO move to GitHub::HTML::ManualWrapFilter or something like that.
     def fix_markdown_quirks(text)
       # Extract pre blocks
@@ -52,15 +44,6 @@ module GitHub::HTML
       text.gsub!(%r{^<pre>.*?</pre>}m) do |match|
         extractions << match
         "{gfm-extraction-#{extractions.size}}"
-      end
-
-      # prevent foo_bar_baz from ending up with an italic word in the middle.
-      # RDiscount has relaxed emphasis support, so only do this when we're using
-      # other markdown implementations.
-      if !rdiscount?
-        text.gsub!(/(^(?! {4}|\t)\w+_\w+_\w[\w_]*)/) do |x|
-          x.gsub('_', '\_') if x.split('').sort.to_s[0..1] == '__'
-        end
       end
 
       # in very clear cases, let newlines become <br /> tags
