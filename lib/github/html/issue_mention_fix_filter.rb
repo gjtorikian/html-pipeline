d91c0ab62e3ce7e3d76ac7574dc9e45e132166b2
Simon Rozet <simon@rozet.name>
2011-03-30 03:23:25 +0200

    extract IssueReference

--- lib/github/html/issue_mention_fix_filter.rb
+++ lib/github/html/issue_mention_fix_filter.rb
@@ -3,9 +3,6 @@ module GitHub
     class IssueMentionFixFilter < Filter
       class Error < StandardError; end
 
-      WORDS = %w[close closes closed fix fixes fixed]
-      REGEX = /(#{Regexp.union(WORDS)}) (gh-|\#)(\d+)\b/i
-
       def call
         doc.search('text()').each do |node|
           content = node.to_html
@@ -13,7 +10,7 @@ module GitHub
           next if !WORDS.any? { |w| content.include?(w) }
           next if node.ancestors('pre, code, a').any?
 
-          html = replace_issue_mention_fix(content)
+          html = IssueReference.auto_link(repository, text, base_url)
 
           next if html == content
 
@@ -22,31 +19,6 @@ module GitHub
       end
 
       private
-
-      def replace_issue_mention_fix(text)
-        text.gsub(REGEX) do |text|
-          number = Integer($3)
-
-          if issue = find_issue(number)
-            "<a href=\"#{issue_url(issue)}\">#{text}</a>"
-          else
-            text
-          end
-        end
-      end
-
-      def find_issue(number)
-        repository.issues.find_by_number(number)
-      end
-
-      def issue_url(issue)
-        File.join(base_url, repository.name_with_owner, "issues",
-          issue.number.to_s)
-      end
-
-      def base_url
-        @context[:base_url] || GitHub::SSLHost
-      end
 
       def repository
         @context[:repository] ||
