5e9ff333d7029014c5d5c1256d19fffef3684080
Ryan Tomayko <rtomayko@gmail.com>
2011-03-31 10:24:33 -0500

    PlainTextInputFilter for commit messages and other plain text inputs

---
+++ lib/github/html/plain_text_input_filter.rb
@@ -0,0 +1,15 @@
+module GitHub::HTML
+  # Simple filter for plain text input. HTML escapes the text input and wraps it
+  # in a div.
+  class PlainTextInputFilter < Filter
+    def initialize(text, context={})
+      raise TypeError, "text cannot be HTML" if text.is_a?(DocumentFragment)
+      @text = "<div>#{EscapeUtils.escape_html(text)}</div>"
+      super(@text, context)
+    end
+
+    def call
+      @doc
+    end
+  end
+end
