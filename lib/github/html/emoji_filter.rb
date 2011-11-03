03662736a4af882b29c85ada4a9863143c46dd2a
Aman Gupta <aman@tmm1.net>
2011-11-03 15:43:23 -0700

    v4 emoji

--- lib/github/html/emoji_filter.rb
+++ lib/github/html/emoji_filter.rb
@@ -116,7 +116,7 @@ module GitHub::HTML
 
       text.gsub EmojiPattern do |match|
         name = $1
-        "<img class='emoji' title=':#{name}:' alt=':#{name}:' src='#{GitHub.asset_host_url}/images/icons/emoji/v3/#{name}.png' height='20' width='20' align='absmiddle' />"
+        "<img class='emoji' title=':#{name}:' alt=':#{name}:' src='#{GitHub.asset_host_url}/images/icons/emoji/v4/#{name}.png' height='20' width='20' align='absmiddle' />"
       end
     end
 
