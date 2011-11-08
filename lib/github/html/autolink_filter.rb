8bbbc84a514a553901c5071a5c218d2cb23c8830
Ryan Tomayko <ryan@github.com>
2011-11-08 08:09:42 -0800

    AutolinkFilter uses Rinku directly instead of AV::TextHelpers#auto_link

--- lib/github/html/autolink_filter.rb
+++ lib/github/html/autolink_filter.rb
@@ -1,14 +1,12 @@
-module GitHub::HTML
-  # HTML Filter for auto_linking urls.
-  class AutolinkFilter < Filter
-    include ActionView::Helpers::UrlHelper
-    include ActionView::Helpers::TagHelper
-    include ActionView::Helpers::TextHelper
+require 'rinku'
 
+module GitHub::HTML
+  # HTML Filter for auto_linking urls in HTML.
+  class AutolinkFilter < Filter
     def call
       return doc if context[:autolink] == false
       html = doc.to_html
-      html = auto_link(html, :link => :urls)
+      html = Rinku.auto_link(html, :urls, nil, %w[a script kbd])
       @doc = parse_html(html)
     end
   end
