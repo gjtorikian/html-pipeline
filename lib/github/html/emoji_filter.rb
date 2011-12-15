4edfd42a89f00949e8881d7ae4e02e6a90339077
Joshua Peek <josh@joshpeek.com>
2011-12-15 12:21:22 -0600

    More emoji! :bowtie:

--- lib/github/html/emoji_filter.rb
+++ lib/github/html/emoji_filter.rb
@@ -1,111 +1,10 @@
+require 'emoji'
+
 module GitHub::HTML
   # HTML filter that replaces :emoji: with images.
   class EmojiFilter < Filter
-    # List of supported emoji as name => code pairs.
-    #
-    # To add emoji:
-    #   - add entries from http://emoji.rubyforge.org/#emoji_table
-    #   - run GitHub::HTML::EmojiFilter.regenerate_assets
-    Emoji = {
-      # Campfire emoji
-      'sunny' => 'e04a',
-      'zap' => 'e13d',
-      'leaves' => 'e447',
-      'lipstick' => 'e31c',
-      'cop' => 'e152',
-      'wheelchair' => 'e20a',
-      'fish' => 'e522',
-      'hammer' => 'e116',
-      'moneybag' => 'e12f',
-      'calling' => 'e104',
-      'memo' => 'e301',
-      'mega' => 'e317',
-      'gift' => 'e112',
-      'pencil' => 'e301',
-      'scissors' => 'e313',
-      'feet' => 'e536',
-      'runner' => 'e115',
-      'heart' => 'e022',
-      'smoking' => 'e30e',
-      'warning' => 'e252',
-      'ok' => 'e24d',
-      'tm' => 'e537',
-      'vs' => 'e12e',
-      'new' => 'e212',
-      'bulb' => 'e10f',
-      'zzz' => 'e13c',
-      'sparkles' => 'e32e',
-      'star' => 'e32f',
-      'mag' => 'e114',
-      'lock' => 'e144',
-      'email' => 'e103',
-      'fist' => 'e010',
-      'v' => 'e011',
-      'punch' => 'e00d',
-      '+1' => 'e00e',
-      'clap' => 'e41f',
-      '-1' => 'e421',
-
-      # More emoji
-      'fire' => 'e11d',
-      'cake' => 'e046',
-      'iphone' => 'e00a',
-      'computer' => 'e00c',
-      'book' => 'e148',
-      'ski' => 'e013',
-      'airplane' => 'e01d',
-      'bus' => 'e159',
-      'train' => 'e01e',
-      'bike' => 'e136',
-      'taxi' => 'e15a',
-      'tophat' => 'e503',
-      'art' => 'e502',
-      'cool' => 'e214',
-      'bomb' => 'e311',
-      'key' => 'e03f',
-      'bear' => 'e051',
-      'beer' => 'e047',
-      'thumbsup' => 'e00e',
-      'thumbsdown' => 'e421',
-      'cloud' => 'e049',
-      'rainbow' => 'e44c',
-      'rose' => 'e032',
-      'sunflower' => 'e305',
-      'apple' => 'e345',
-      'lips' => 'e41c',
-      'nail_care' => 'e31d',
-      'couple' => 'e428',
-      'princess' => 'e51c',
-      'dog' => 'e052',
-      'cat' => 'e04f',
-      'horse' => 'e01a',
-      'dolphin' => 'e520',
-      'koala' => 'e527',
-      'kiss' => 'e003',
-      'broken_heart' => 'e023',
-      'poop' => 'e05a',
-
-      # Custom emoji
-      'octocat' => nil,
-      'metal' => nil,
-      'shipit' => nil,
-      'squirrel' => nil,
-
-      # Doom emoji
-      'feelsgood' => nil,
-      'finnadie' => nil,
-      'goberserk' => nil,
-      'godmode' => nil,
-      'hurtrealbad' => nil,
-      'rage1' => nil,
-      'rage2' => nil,
-      'rage3' => nil,
-      'rage4' => nil,
-      'suspect' => nil
-    }
-
     # Build a regexp that matches all valid :emoji: names.
-    EmojiPattern = /:(#{Emoji.map{ |name,code| Regexp.escape(name) }.join('|')}):/
+    EmojiPattern = /:(#{Emoji.names.map { |name| Regexp.escape(name) }.join('|')}):/
 
     def call
       doc.search('text()').each do |node|
@@ -129,33 +28,7 @@ module GitHub::HTML
 
       text.gsub EmojiPattern do |match|
         name = $1
-        "<img class='emoji' title=':#{name}:' alt=':#{name}:' src='#{GitHub.asset_host_url}/images/icons/emoji/#{name}.png?v4' height='20' width='20' align='absmiddle' />"
-      end
-    end
-
-    # Regenerate css/gif assets for all entries in Emoji.
-    #
-    # dir - a String path to the emoji-css-builder source
-    #
-    # If the directory provided does not exist, emoji-css-builder will
-    # be checked out there. You will probably still need to install
-    # ghostscript and other dependencies manually.
-    #
-    # Returns nothing.
-    def self.regenerate_assets(dir = '~/Dropbox/GitHub/stock/Emoji/')
-      require 'fileutils'
-      dir = File.expand_path(dir)
-
-      if !File.exists?(dir)
-        raise 'Could not find emoji source'
-      end
-
-      Emoji.each do |name, code|
-        next unless code
-        file = File.join(dir, "kb-emoji-U+#{code.upcase}@2x.png")
-
-        raise "Unknown emoji: #{name}" unless File.exists?(file)
-        FileUtils.cp file, RAILS_ROOT+"/public/images/icons/emoji/#{name}.png"
+        "<img class='emoji' title=':#{name}:' alt=':#{name}:' src='#{GitHub.asset_host_url}/images/icons/emoji/#{name}.png?v5' height='20' width='20' align='absmiddle' />"
       end
     end
   end
