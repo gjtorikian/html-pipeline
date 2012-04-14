5f95669c7050fcb534f42d6b130e2b7af44028fb
Vicent Martí <tanoku@gmail.com>
2012-04-13 17:42:17 -0700

    Anchor all <h*> elements in READMEs and blobs
    Fixes #3441

---
+++ lib/github/html/toc_filter.rb
@@ -0,0 +1,24 @@
+module GitHub::HTML
+  # HTML filter that adds a 'name' attribute to all headers
+  # in a document, so they can be accessed from a table of contents
+  #
+  # TODO: besides adding the name attribute, we should get around to
+  # eventually generating the Table of Contents itself, with links
+  # to each header
+  class TableOfContentsFilter < Filter
+    def call
+      headers = Hash.new(0)
+      doc.css('h1, h2, h3, h4, h5, h6').each do |node|
+        name = node.text.downcase
+        name.gsub!(/[^\w\- ]/, '') # remove punctuation
+        name.gsub!(' ', '-') # replace spaces with dash
+
+        uniq = (headers[name] > 0) ? "-#{headers[name]}" : ''
+        headers[name] += 1
+
+        node.before "<a name=\"#{name}#{uniq}\"></a>"
+      end
+      doc
+    end
+  end
+end
