40761ddbc0aa0bf8cdb501ec9c6e268f555a1ec7
Jon Rohan <rohan@github.com>
2012-05-16 18:38:40 -0700

    toc_filter got messed up

--- lib/github/html/toc_filter.rb
+++ lib/github/html/toc_filter.rb
@@ -1,26 +1,26 @@
-
-          header_content.add_previous_sibling(%Q{<a name="#{name}#{uniq}" class="anchor" href="##{name}#{uniq}"><span class="mini-icon-link"></span></a>})
-        end
-        headers[name] += 1
-        if header_content = node.children.first
-        name = EscapeUtils.escape_uri(name) # escape extended UTF-8 chars
-        name = node.text.downcase
-        name.gsub!(' ', '-') # replace spaces with dash
-        name.gsub!(/[^\w\- ]/, '') # remove punctuation
-        uniq = (headers[name] > 0) ? "-#{headers[name]}" : ''
-      doc
-      doc.css('h1, h2, h3, h4, h5, h6').each do |node|
-      end
-      headers = Hash.new(0)
-    def call
-    end
-  #
-  # eventually generating the Table of Contents itself, with links
+module GitHub::HTML
   # HTML filter that adds a 'name' attribute to all headers
   # in a document, so they can be accessed from a table of contents
-  # to each header
+  #
   # TODO: besides adding the name attribute, we should get around to
+  # eventually generating the Table of Contents itself, with links
+  # to each header
   class TableOfContentsFilter < Filter
+    def call
+      headers = Hash.new(0)
+      doc.css('h1, h2, h3, h4, h5, h6').each do |node|
+        name = node.text.downcase
+        name.gsub!(/[^\w\- ]/, '') # remove punctuation
+        name.gsub!(' ', '-') # replace spaces with dash
+        name = EscapeUtils.escape_uri(name) # escape extended UTF-8 chars
+
+        uniq = (headers[name] > 0) ? "-#{headers[name]}" : ''
+        headers[name] += 1
+        if header_content = node.children.first
+          header_content.add_previous_sibling(%Q{<a name="#{name}#{uniq}" class="anchor" href="##{name}#{uniq}"><span class="mini-icon-link"></span></a>})
+        end
+      end
+      doc
+    end
   end
 end
-module GitHub::HTML
