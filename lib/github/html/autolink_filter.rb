9c7395f3d1288126962fbd88f64321a43dec91fd
Vicent Marti <tanoku@gmail.com>
2012-07-08 15:24:26 +0200

    Allow short-URL (intranet) autolinks in Enterprise
    Fixes #4045

--- lib/github/html/autolink_filter.rb
+++ lib/github/html/autolink_filter.rb
@@ -5,7 +5,13 @@ module GitHub::HTML
   class AutolinkFilter < Filter
     def call
       return html if context[:autolink] == false
-      Rinku.auto_link(html, :urls, nil, %w[a script kbd pre code])
+      flags = 0
+
+      if GitHub.enterprise?
+        flags |= Rinku::AUTOLINK_SHORT_DOMAINS
+      end
+
+      Rinku.auto_link(html, :urls, nil, %w[a script kbd pre code], flags)
     end
   end
 end
