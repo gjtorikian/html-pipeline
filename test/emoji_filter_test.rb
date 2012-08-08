b13f4a13e20ae6104e55ef653d16617e2a6a2d3b
Jerry Cheung <jch@whatcodecraves.com>
2012-08-08 13:02:22 -0700

    EmojiFilter customize asset_root to be asset pipeline friendly

--- test/emoji_filter_test.rb
+++ test/emoji_filter_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class GitHub::HTML::EmojiFilterTest < Test::Unit::TestCase
   def test_emojify
-    filter = GitHub::HTML::EmojiFilter.new("<p>:shipit:</p>")
+    filter = GitHub::HTML::EmojiFilter.new("<p>:shipit:</p>", {:asset_root => 'https://foo.com'})
     doc = filter.call
-    assert_match %r{emoji/shipit.png}, doc.to_html
+    assert_match "https://foo.com/emoji/shipit.png", doc.search('img').attr('src').value
   end
 end
