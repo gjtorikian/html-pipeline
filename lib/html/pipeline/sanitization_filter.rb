36ac207393a8f5ccf3710357431888726944c1d7
Ryan Tomayko <rtomayko@gmail.com>
2012-09-12 02:04:40 -0700

    whitelist <div> through sanitization filter
    No idea how this has never come up before. Literal <div>s are
    perfectly legal markdown and cause the containing markup not to be
    processed as markdown. Looks like we're stripping them in
    sanitization here though which would cause a bunch of problems.
    
    I'm whitelisting for other reasons: HTML email often include <div>s
    and we need to retain them for proper layout in discussions.
    Hopefully this won't cause problems elsewhere.

--- lib/html/pipeline/sanitization_filter.rb
+++ lib/html/pipeline/sanitization_filter.rb
@@ -31,7 +31,7 @@ module HTML::Pipeline
     WHITELIST = {
       :elements => %w(
         h1 h2 h3 h4 h5 h6 h7 h8 br b i strong em a pre code img tt
-        ins del sup sub p ol ul table blockquote dl dt dd
+        div ins del sup sub p ol ul table blockquote dl dt dd
         kbd q samp var hr ruby rt rp
       ),
       :attributes => {
