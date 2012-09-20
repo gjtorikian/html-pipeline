8c64b0b92f5d2c5e59c54737dcfc74e016309ac3
Jerry Cheung <jch@whatcodecraves.com>
2012-09-20 11:47:15 -0700

    fix plain_text_input_filter to not escape '/' by default

--- lib/github/html/plain_text_input_filter.rb
+++ lib/github/html/plain_text_input_filter.rb
@@ -1,9 +1,11 @@
+require 'escape_utils'
+
 module GitHub::HTML
   # Simple filter for plain text input. HTML escapes the text input and wraps it
   # in a div.
   class PlainTextInputFilter < TextFilter
     def call
-      "<div>#{EscapeUtils.escape_html(@text)}</div>"
+      "<div>#{EscapeUtils.escape_html(@text, false)}</div>"
     end
   end
 end
