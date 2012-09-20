364fb51c3950781095febfb55294e8c087827d09
Jerry Cheung <jch@whatcodecraves.com>
2012-09-20 11:43:29 -0700

    document and parameterize 'flags' context option for autolink_filter

--- lib/github/html/autolink_filter.rb
+++ lib/github/html/autolink_filter.rb
@@ -2,14 +2,17 @@ require 'rinku'
 
 module GitHub::HTML
   # HTML Filter for auto_linking urls in HTML.
+  #
+  # Context options:
+  #   :autolink - boolean whether to autolink urls
+  #   :flags    - additional Rinku flags. See https://github.com/vmg/rinku
+  #
+  # This filter does not write additional information to the context.
   class AutolinkFilter < Filter
     def call
       return html if context[:autolink] == false
       flags = 0
-
-      if GitHub.enterprise?
-        flags |= Rinku::AUTOLINK_SHORT_DOMAINS
-      end
+      flags |= context[:flags] if context[:flags]
 
       Rinku.auto_link(html, :urls, nil, %w[a script kbd pre code], flags)
     end
--- test/unit/html/autolink_filter_test.rb
+++ test/unit/html/autolink_filter_test.rb
@@ -9,4 +9,14 @@ class GitHub::HTML::AutolinkFilterTest < Test::Unit::TestCase
     assert_equal '<p>"<a href="http://www.github.com">http://www.github.com</a>"</p>',
       AutolinkFilter.to_html('<p>"http://www.github.com"</p>')
   end
+
+  def test_autolink_option
+    assert_equal '<p>"http://www.github.com"</p>',
+      AutolinkFilter.to_html('<p>"http://www.github.com"</p>', :autolink => false)
+  end
+
+  def test_autolink_flags
+    assert_equal '<p>"<a href="http://github">http://github</a>"</p>',
+      AutolinkFilter.to_html('<p>"http://github"</p>', :flags => Rinku::AUTOLINK_SHORT_DOMAINS)
+  end
 end
