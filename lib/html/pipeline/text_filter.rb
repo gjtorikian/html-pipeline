70c7ac5b8874a4da6021e892ce573d3579a6f8f9
Chris Wanstrath <chris@ozmm.org>
2012-09-24 13:38:36 -0700

    No try()

--- lib/html/pipeline/text_filter.rb
+++ lib/html/pipeline/text_filter.rb
@@ -5,7 +5,7 @@ module HTML::Pipeline
     def initialize(text, context = nil, result = nil)
       raise TypeError, "text cannot be HTML" if text.is_a?(DocumentFragment)
       # Ensure that this is always a string
-      @text = text.try(:to_str) || text.to_s
+      @text = text.respond_to?(:to_str) ? text.to_str : text.to_s
       super nil, context, result
     end
   end
