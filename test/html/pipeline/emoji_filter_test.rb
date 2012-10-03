68379ea54ac74542b8148e65f5649db74eb02f2a
Julia West <julia@github.com>
2012-10-03 18:25:23 -0400

    Ensure required emoji context values are present. - Raise an ArgumentError if the asset_root isn't provided.

--- test/html/pipeline/emoji_filter_test.rb
+++ test/html/pipeline/emoji_filter_test.rb
@@ -6,4 +6,11 @@ class HTML::Pipeline::EmojiFilterTest < Test::Unit::TestCase
     doc = filter.call
     assert_match "https://foo.com/emoji/shipit.png", doc.search('img').attr('src').value
   end
+
+  def test_missing_context
+    filter = HTML::Pipeline::EmojiFilter.new("<p>:shipit:</p>", {})
+    assert_raises ArgumentError do
+      filter.call
+    end
+  end
 end
