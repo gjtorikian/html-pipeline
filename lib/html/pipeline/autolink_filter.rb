d77e92f900038983e0605f3d08bc95186f39a1e4
Wynn Netherland <wynn@github.com>
2013-07-10 14:32:19 -0500

    Allow passing skip_tags in autolink filter context
    Enables passing the `skip_tags` argument to Rinku. Defaults to
    Rinku.skip_tags

--- lib/html/pipeline/autolink_filter.rb
+++ lib/html/pipeline/autolink_filter.rb
@@ -5,17 +5,21 @@ module HTML
     # HTML Filter for auto_linking urls in HTML.
     #
     # Context options:
-    #   :autolink - boolean whether to autolink urls
-    #   :flags    - additional Rinku flags. See https://github.com/vmg/rinku
+    #   :autolink  - boolean whether to autolink urls
+    #   :skip_tags - HTML tags inside which autolinking will be skipped.
+    #                See Rinku.skip_tags
+    #   :flags     - additional Rinku flags. See https://github.com/vmg/rinku
     #
     # This filter does not write additional information to the context.
     class AutolinkFilter < Filter
       def call
         return html if context[:autolink] == false
+
+        skip_tags = context[:skip_tags]
         flags = 0
         flags |= context[:flags] if context[:flags]
 
-        Rinku.auto_link(html, :urls, nil, %w[a script kbd pre code], flags)
+        Rinku.auto_link(html, :urls, nil, skip_tags, flags)
       end
     end
   end
