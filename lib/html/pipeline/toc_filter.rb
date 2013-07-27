bb203f2973774851837f787f626dd81db481944c
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-07-26 20:27:36 -0700

    Keep TableOfContentsFilter interface consistent with other filters by removing attr_reader. Also, reduce complexity by removing private methods and keep changes within #call. Finally, implement #build_toc to wrap result[:toc] in an unordered list.

--- lib/html/pipeline/toc_filter.rb
+++ lib/html/pipeline/toc_filter.rb
@@ -25,13 +25,8 @@ module HTML
     class TableOfContentsFilter < Filter
       PUNCTUATION_REGEXP = RUBY_VERSION > "1.9" ? /[^\p{Word}\- ]/u : /[^\w\- ]/
 
-      # Public: Gets the String value of the Table of Contents.
-      attr_reader :toc
-
       def call
         result[:toc] = ""
-
-        @toc = ""
 
         headers = Hash.new(0)
         doc.css('h1, h2, h3, h4, h5, h6').each do |node|
@@ -43,27 +38,17 @@ module HTML
           uniq = (headers[name] > 0) ? "-#{headers[name]}" : ''
           headers[name] += 1
           if header_content = node.children.first
-            append_toc text, name, uniq
-            add_header_anchor header_content, name, uniq
+            result[:toc] << %Q{<li><a href="##{name}#{uniq}">#{text}</a></li>\n}
+            header_content.add_previous_sibling(%Q{<a name="#{name}#{uniq}" class="anchor" href="##{name}#{uniq}"><span class="octicon octicon-link"></span></a>})
           end
         end
-        finalize_toc unless toc.empty?
+        build_toc
         doc
       end
 
-      # Private: Add header link to Table of Contents list.
-      def append_toc(text, name, uniq)
-        @toc << %Q{<li><a href="##{name}#{uniq}">#{text}</a></li>\n}
-      end
-
-      # Private: Add an anchor with 'name' attribute to a header.
-      def add_header_anchor(header_content, name, uniq)
-        header_content.add_previous_sibling(%Q{<a name="#{name}#{uniq}" class="anchor" href="##{name}#{uniq}"><span class="octicon octicon-link"></span></a>})
-      end
-
       # Private: Wrap Table of Contents list items within a container.
-      def finalize_toc
-        result[:toc] = %Q{<ul class="section-nav">\n#{toc}</ul>}
+      def build_toc
+        result[:toc] = %Q{<ul class="section-nav">\n#{result[:toc]}</ul>} unless result[:toc].empty?
       end
     end
   end
