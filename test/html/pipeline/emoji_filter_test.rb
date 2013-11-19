90d51ef6d3f76fb2c4ac1581a7876c9802d9daf2
Joel Oliveira <joel.oliveira@gmail.com>
2013-11-19 12:39:41 -0500

    CGI.escape emoji name
    The commonly used `+1`, when located somewhere like S3, will not work
    as the + is not escaped, causing the browser to think it's a space
    instead of a literal `+`. To replicate, upload +1.png to S3 and try to
    bring it up in Chrome.
    
    This commit escapes the emoji name before returning the img tag.

--- test/html/pipeline/emoji_filter_test.rb
+++ test/html/pipeline/emoji_filter_test.rb
@@ -9,6 +9,12 @@ class HTML::Pipeline::EmojiFilterTest < Test::Unit::TestCase
     assert_match "https://foo.com/emoji/shipit.png", doc.search('img').attr('src').value
   end
   
+  def test_uri_encoding
+    filter = EmojiFilter.new("<p>:+1:</p>", {:asset_root => 'https://foo.com'})
+    doc = filter.call
+    assert_match "https://foo.com/emoji/%2B1.png", doc.search('img').attr('src').value
+  end
+  
   def test_required_context_validation
     exception = assert_raise(ArgumentError) { 
       EmojiFilter.call("", {}) 
