75b724a50ea8b64cf9a999f9ee57f963ef5b6e28
Joshua Peek <josh@joshpeek.com>
2014-01-08 16:35:29 -0800

    Camo SSL images

--- lib/html/pipeline/camo_filter.rb
+++ lib/html/pipeline/camo_filter.rb
@@ -23,17 +23,13 @@ module HTML
           next if element['src'].nil?
           src = element['src'].strip
           src = src.sub(%r!^http://github.com!, 'https://github.com')
-          next if context[:disable_asset_proxy]
 
-          if src =~ /^http:/ || src =~ /^https:\/\/img.skitch.com\//
-            element['src'] = asset_proxy_url(src)
-          else
-            element['src'] = src
-          end
+          next if context[:disable_asset_proxy]
+          element['src'] = asset_proxy_url(src)
         end
         doc
       end
-      
+
       # Implementation of validate hook.
       # Errors should raise exceptions or use an existing validator.
       def validate
