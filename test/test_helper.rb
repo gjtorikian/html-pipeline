2d541f32d5d1363b9163b288c80a4da0c5a350d0
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2014-07-05 12:46:08 -0700

    Use MiniTest in Test Helper
    - require MiniTest
    - Bring back `assert_equal_html` and mixin to MiniTest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,36 +1,11 @@
 require 'bundler/setup'
 require 'html/pipeline'
-
-# Force Ruby 1.9 to use test-unit gem instead of StdLib test-unit which wraps minitest
-gem 'test-unit'
-require 'test/unit'
+require 'minitest/autorun'
 
 require 'active_support/core_ext/string'
 require 'active_support/core_ext/object/try'
 
 module TestHelpers
-  # Asserts that `needle` is not a member of `haystack`, where
-  # `haystack` is any object that responds to `include?`.
-  def assert_doesnt_include(needle, haystack, message = nil)
-    error = '<?> included in <?>'
-    message = build_message(message, error, needle.to_s, Array(haystack).map(&:to_s))
-
-    assert_block message do
-      !haystack.include?(needle)
-    end
-  end
-
-  # Asserts that `needle` is a member of `haystack`, where
-  # `haystack` is any object that responds to `include?`.
-  def assert_includes(needle, haystack, message = nil)
-    error = '<?> not included in <?>'
-    message = build_message(message, error, needle.to_s, Array(haystack).map(&:to_s))
-
-    assert_block message do
-      haystack.include?(needle)
-    end
-  end
-
   # Asserts that two html fragments are equivalent. Attribute order
   # will be ignored.
   def assert_equal_html(expected, actual)
@@ -39,4 +14,4 @@ module TestHelpers
   end
 end
 
-Test::Unit::TestCase.send(:include, TestHelpers)
+MiniTest::Test.send(:include, TestHelpers)
