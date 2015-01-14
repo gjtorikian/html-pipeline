9926f40857e2f8f65a2905f2a3075e99d8fa87d9
Juanito Fatas <katehuang0320@gmail.com>
2015-01-14 22:41:04 +0800

    Implement ignored_ancestor_tags to accept more ignored ancestor tags.

--- test/html/pipeline/emoji_filter_test.rb
+++ test/html/pipeline/emoji_filter_test.rb
@@ -2,22 +2,22 @@ require 'test_helper'
 
 class HTML::Pipeline::EmojiFilterTest < Minitest::Test
   EmojiFilter = HTML::Pipeline::EmojiFilter
-  
+
   def test_emojify
     filter = EmojiFilter.new("<p>:shipit:</p>", {:asset_root => 'https://foo.com'})
     doc = filter.call
     assert_match "https://foo.com/emoji/shipit.png", doc.search('img').attr('src').value
   end
-  
+
   def test_uri_encoding
     filter = EmojiFilter.new("<p>:+1:</p>", {:asset_root => 'https://foo.com'})
     doc = filter.call
     assert_match "https://foo.com/emoji/%2B1.png", doc.search('img').attr('src').value
   end
-  
+
   def test_required_context_validation
-    exception = assert_raises(ArgumentError) { 
-      EmojiFilter.call("", {}) 
+    exception = assert_raises(ArgumentError) {
+      EmojiFilter.call("", {})
     }
     assert_match /:asset_root/, exception.message
   end
@@ -45,6 +45,20 @@ class HTML::Pipeline::EmojiFilterTest < Minitest::Test
   def test_not_emojify_in_pre_tags
     body = "<pre>:shipit:</pre>"
     filter = EmojiFilter.new(body, {:asset_root => 'https://foo.com'})
+    doc = filter.call
+    assert_equal body, doc.to_html
+  end
+
+  def test_not_emojify_in_custom_single_tag_foo
+    body = "<foo>:shipit:</foo>"
+    filter = EmojiFilter.new(body, {:asset_root => 'https://foo.com', ignored_ancestor_tags: %w(foo)})
+    doc = filter.call
+    assert_equal body, doc.to_html
+  end
+
+  def test_not_emojify_in_custom_multiple_tags_foo_and_bar
+    body = "<bar>:shipit:</bar>"
+    filter = EmojiFilter.new(body, {:asset_root => 'https://foo.com', ignored_ancestor_tags: %w(foo bar)})
     doc = filter.call
     assert_equal body, doc.to_html
   end
