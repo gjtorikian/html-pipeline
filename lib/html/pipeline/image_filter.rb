139993ffcebfd6cdc8cab700d9093a1152be04ca
Stan Low <a22301613@yahoo.com.tw>
2015-07-21 15:17:04 +0800

    Add test

--- lib/html/pipeline/image_filter.rb
+++ lib/html/pipeline/image_filter.rb
@@ -2,7 +2,7 @@ module HTML
   class Pipeline
     class ImageFilter < TextFilter
       def call
-        @text.gsub(/https?:\/\/.+\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
+        @text.gsub(/(https|http)?:\/\/.+\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
         %|<img src="#{match}" alt=""/>|
         end
       end
