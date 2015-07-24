c2627f5fe3fc9c5f1a0164654612c6213792c762
Stan Low <a22301613@yahoo.com.tw>
2015-07-24 15:25:50 +0800

    Add youtube filter

--- lib/html/pipeline.rb
+++ lib/html/pipeline.rb
@@ -41,6 +41,7 @@ module HTML
     autoload :TextileFilter,         'html/pipeline/textile_filter'
     autoload :TableOfContentsFilter, 'html/pipeline/toc_filter'
     autoload :TextFilter,            'html/pipeline/text_filter'
+    autoload :YoutubeFilter,         'html/pipeline/youtube_filter'
 
     # Our DOM implementation.
     DocumentFragment = Nokogiri::HTML::DocumentFragment
---
+++ test/html/pipeline/youtube_filter_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+YoutubeFilter = HTML::Pipeline::YoutubeFilter
+
+class HTML::Pipeline::YoutubeFilterTest < Minitest::Test
+  def filter(html)
+    YoutubeFilter.to_html(html)
+  end
+
+  def test_transform_with_default_value
+    assert_equal %(<div class="video youtube"><iframe width="420" height="315" src="//www.youtube.com/embed/Kg4aWWIsszw" frameborder="0" allowfullscreen></iframe></div>),
+    YoutubeFilter.to_html(%(https://www.youtube.com/watch?v=Kg4aWWIsszw))
+  end
+
+  def test_transform_with_custom_context_value
+    assert_equal %(<div class="video youtube"><iframe width="500" height="100" src="//www.youtube.com/embed/Kg4aWWIsszw?autoplay=1&rel=0" frameborder="5" allowfullscreen></iframe></div>),
+    YoutubeFilter.to_html(%(https://www.youtube.com/watch?v=Kg4aWWIsszw), video_width: 500, video_height: 100, video_frameborder: 5, video_autoplay: true, video_hide_related: true)
+  end
+end
