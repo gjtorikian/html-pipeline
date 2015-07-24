566640524f4d35e7372f3fc6b61e927d5229d78f
Stan Low <a22301613@yahoo.com.tw>
2015-07-24 16:21:36 +0800

    Add basic flickr filter

---
+++ lib/html/pipeline/flickr_filter.rb
@@ -0,0 +1,37 @@
+require 'uri'
+require 'net/http'
+
+module HTML
+  class Pipeline
+    class FlickrFilter < TextFilter
+      def call
+        regex = %r{https?://(www\.)?flickr\.com/photos/[^\s<]*}
+        uri = URI("https://www.flickr.com/services/oembed")
+
+        link_attr = context[:flickr_link_attr] || ""
+
+        @text.gsub(regex) do |match|
+          params = {
+            url: match,
+            format: :json,
+            maxwidth: max_value(:width),
+            maxheight: max_value(:height)
+          }
+
+          uri.query = URI.encode_www_form(params)
+
+          response = JSON.parse(Net::HTTP.get(uri))
+
+          %{<a href="#{match}" #{link_attr}><img src="#{response["url"]}" alt="#{response["title"]}" title="#{response["title"]}" /></a>}
+        end
+      end
+
+      private
+
+      def max_value(attr)
+        value = context["max#{attr}".to_sym]
+        value.to_i >= 0 ? value.to_i : 0
+      end
+    end
+  end
+end
