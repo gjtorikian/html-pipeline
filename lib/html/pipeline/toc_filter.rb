4b9168d837cde21c51150eb7488a62fcc62e6fb8
Charlie Somerville <charlie@charlie.bz>
2017-01-12 15:53:10 +1100

    only downcase ascii characters on ruby 2.4+
    Starting in Ruby 2.4, the String case conversion methods are Unicode
    aware. To ensure that anchors in TOC links remain stable when upgrading
    to Ruby 2.4, we need to preserve the old behaviour by passing :ascii
    to String#downcase

--- lib/html/pipeline/toc_filter.rb
+++ lib/html/pipeline/toc_filter.rb
@@ -36,7 +36,7 @@ module HTML
         headers = Hash.new(0)
         doc.css('h1, h2, h3, h4, h5, h6').each do |node|
           text = node.text
-          id = text.downcase
+          id = ascii_downcase(text)
           id.gsub!(PUNCTUATION_REGEXP, '') # remove punctuation
           id.gsub!(' ', '-') # replace spaces with dash
 
@@ -49,6 +49,16 @@ module HTML
         end
         result[:toc] = %Q{<ul class="section-nav">\n#{result[:toc]}</ul>} unless result[:toc].empty?
         doc
+      end
+
+      if RUBY_VERSION >= "2.4"
+        def ascii_downcase(str)
+          str.downcase(:ascii)
+        end
+      else
+        def ascii_downcase(str)
+          str.downcase
+        end
       end
     end
   end
