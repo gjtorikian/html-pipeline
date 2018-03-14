e1468978fe66824750c536a156e40767e5465e17
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2018-03-14 14:29:47 -0400

    Add the whole module pathname

--- test/html/pipeline/require_helper_test.rb
+++ test/html/pipeline/require_helper_test.rb
@@ -13,14 +13,14 @@ class HTML::Pipeline::RequireHelperTest < Minitest::Test
   end
 
   def test_raises_error_including_message
-    error = assert_raises(MissingDependencyError) do
+    error = assert_raises(HTML::Pipeline::MissingDependencyError) do
       HTML::Pipeline.require_dependency('non-existant', 'SomeClass')
     end
     assert_includes(error.message, "Missing dependency 'non-existant' for SomeClass. See README.md for details.")
   end
 
   def test_raises_error_includes_underlying_message
-    error = assert_raises MissingDependencyError do
+    error = assert_raises HTML::Pipeline::MissingDependencyError do
       HTML::Pipeline.require_dependency('non-existant', 'SomeClass')
     end
     assert_includes(error.message, 'LoadError: cannot load such file')
