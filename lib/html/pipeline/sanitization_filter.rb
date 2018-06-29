3f8702d72543674dfa4d60af58a3f045d7ae8709
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2018-06-29 11:06:39 -0400

    Whitelist some accessibility properties

--- lib/html/pipeline/sanitization_filter.rb
+++ lib/html/pipeline/sanitization_filter.rb
@@ -41,7 +41,8 @@ module HTML
         elements: %w[
           h1 h2 h3 h4 h5 h6 h7 h8 br b i strong em a pre code img tt
           div ins del sup sub p ol ul table thead tbody tfoot blockquote
-          dl dt dd kbd q samp var hr ruby rt rp li tr td th s strike summary details
+          dl dt dd kbd q samp var hr ruby rt rp li tr td th s strike summary
+          details caption figure figcaption
         ],
         remove_contents: ['script'],
         attributes: {
@@ -52,23 +53,24 @@ module HTML
           'del'        => ['cite'],
           'ins'        => ['cite'],
           'q'          => ['cite'],
-         :all => ['abbr', 'accept', 'accept-charset',
-                  'accesskey', 'action', 'align', 'alt', 'axis',
-                  'border', 'cellpadding', 'cellspacing', 'char',
-                  'charoff', 'charset', 'checked',
-                  'clear', 'cols', 'colspan', 'color',
-                  'compact', 'coords', 'datetime', 'dir',
-                  'disabled', 'enctype', 'for', 'frame',
-                  'headers', 'height', 'hreflang',
-                  'hspace', 'ismap', 'label', 'lang',
-                  'maxlength', 'media', 'method',
-                  'multiple', 'name', 'nohref', 'noshade',
-                  'nowrap', 'open', 'prompt', 'readonly', 'rel', 'rev',
-                  'rows', 'rowspan', 'rules', 'scope',
-                  'selected', 'shape', 'size', 'span',
-                  'start', 'summary', 'tabindex', 'target',
-                  'title', 'type', 'usemap', 'valign', 'value',
-                  'vspace', 'width', 'itemprop']
+          all: %w[abbr accept accept-charset
+                  accesskey action align alt
+                  aria-describedby aria-hidden aria-label aria-labelledby
+                  axis border cellpadding cellspacing char
+                  charoff charset checked
+                  clear cols colspan color
+                  compact coords datetime dir
+                  disabled enctype for frame
+                  headers height hreflang
+                  hspace ismap label lang
+                  maxlength media method
+                  multiple name nohref noshade
+                  nowrap open prompt readonly rel rev
+                  rows rowspan rules scope
+                  selected shape size span
+                  start summary tabindex target
+                  title type usemap valign value
+                  vspace width itemprop]
         },
         protocols: {
           'a'          => { 'href' => ANCHOR_SCHEMES },
