c594b6660d11cdb90422ccf0562f951ba42a8a37
Juanito Fatas <katehuang0320@gmail.com>
2019-12-19 22:20:55 +0900

    Improve documents of SyntaxHighlightFilter

--- lib/html/pipeline/syntax_highlight_filter.rb
+++ lib/html/pipeline/syntax_highlight_filter.rb
@@ -4,8 +4,15 @@ HTML::Pipeline.require_dependency('rouge', 'SyntaxHighlightFilter')
 
 module HTML
   class Pipeline
-    # HTML Filter that syntax highlights code blocks wrapped
-    # in <pre lang="...">.
+    # HTML Filter that syntax highlights text inside code blocks.
+    #
+    # Context options:
+    #
+    #   :highlight => String represents the language to pick lexer. Defaults to empty string.
+    #   :scope => String represents the class attribute adds to pre element after.
+    #             Defaults to "highlight highlight-css" if highlights a css code block.
+    #
+    # This filter does not write any additional information to the context hash.
     class SyntaxHighlightFilter < Filter
       def initialize(*args)
         super(*args)
