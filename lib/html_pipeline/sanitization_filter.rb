aecaad396d4621598a938593ff20d7a14a83ef1f
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2022-12-26 21:48:19 -0800

    Add picture/source
    Closes https://github.com/gjtorikian/html-pipeline/issues/352

--- lib/html_pipeline/sanitization_filter.rb
+++ lib/html_pipeline/sanitization_filter.rb
@@ -19,10 +19,10 @@ class HTMLPipeline
     # allowed through by default.
     DEFAULT_CONFIG = Selma::Sanitizer::Config.freeze_config({
       elements: ["h1", "h2", "h3", "h4", "h5", "h6", "br", "b", "i", "strong", "em", "a", "pre", "code",
-                 "img", "tt", "div", "ins", "del", "sup", "sub", "p", "ol", "ul", "table", "thead", "tbody", "tfoot",
+                 "img", "tt", "div", "ins", "del", "sup", "sub", "p", "picture", "ol", "ul", "table", "thead", "tbody", "tfoot",
                  "blockquote", "dl", "dt", "dd", "kbd", "q", "samp", "var", "hr", "ruby", "rt", "rp", "li", "tr", "td", "th",
                  "s", "strike", "summary", "details", "caption", "figure", "figcaption", "abbr", "bdo", "cite",
-                 "dfn", "mark", "small", "span", "time", "wbr",],
+                 "dfn", "mark", "small", "source", "span", "time", "wbr",],
 
       attributes: {
         "a" => ["href"],
@@ -32,6 +32,7 @@ class HTMLPipeline
         "del" => ["cite"],
         "ins" => ["cite"],
         "q" => ["cite"],
+        "source" => ["srcset"],
         all: ["abbr", "accept", "accept-charset", "accesskey", "action", "align", "alt", "aria-describedby",
               "aria-hidden", "aria-label", "aria-labelledby", "axis", "border", "char",
               "charoff", "charset", "checked", "clear", "cols", "colspan", "compact", "coords", "datetime", "dir",
